eb3d75b0456099e63a51e1ddf10f8d18f083b42b
IT20642914 <avishkachanaka@gmail.com>
2024-08-13 15:23:57 +0530

    chore: Remove unnecessary code in JokeRepository

--- src/jokes/repositories/joke.repository.ts
+++ src/jokes/repositories/joke.repository.ts
@@ -2,6 +2,4 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Joke } from '../entities/joke';
 
 @EntityRepository(Joke)
-export class JokeRepository extends Repository<Joke> {
-
-}
+export class JokeRepository extends Repository<Joke> {}
