0d84ac64761afaf2815170d7b51e244886bc502d
IT20642914 <avishkachanaka@gmail.com>
2024-08-14 07:18:23 +0530

    chore: Refactor code to use environment variables for database configuration and remove hardcoded password

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -6,7 +6,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT, 10) || 3306,
   username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'Avishka.99',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_DATABASE || 'jokes_db',
   entities: [Joke],
   synchronize: true
